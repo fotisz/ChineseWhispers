76851da23ed5208992d3d6c43adf2527a7719207
Fotis <fotisz@users.noreply.github.com>
2017-08-04 19:29:38 +0300

    Update Island.js

--- js/Island.js
+++ js/Island.js
@@ -4,7 +4,7 @@ function Island(code, model) {
   this.model = model;
   this.name = Math.random();
   this.prefix = choiceRandom([
-    'a', 'b', 'z', 'd', 'ü', 'l'
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
   ]);
   this.words = {};
 }
